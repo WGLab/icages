25a4f87058dcb6f15feccac67860b7c4ecfcacc5
coco90417 <coco90417@gmail.com>
2015-03-05 20:32:41 -0800

    change dgidb back

--- bin/icagesDrug.pl
+++ bin/icagesDrug.pl
@@ -135,7 +135,7 @@ sub getDrugs{
     %onc = %{$oncRef};
     %sup = %{$supRef};
     @seeds = keys %neighbors;
-    $callDgidb = $icagesLocation . "bin/DGIdb/getDrugList.pl";
+    $callDgidb = $icagesLocation . "bin/DGIdb/perl_example.pl";
     $supFile = $rawInputFile . ".suppressor.drug";
     $oncFile = $rawInputFile . ".oncogene.drug";
     $otherFile = $rawInputFile . ".other.drug";
@@ -152,13 +152,13 @@ sub getDrugs{
     $onc = join(",", @onc);
     $other = join(",", @other);
     if($sup ne ""){
-        !system("$callDgidb --genes='$sup' --interaction_type='activator,other/unknown,n/a,inducer,stimulator' --source_trust_levels='Expert curated' --output='$supFile'") or die "ERROR: cannot get drugs\n";
+        !system("perl $callDgidb --genes='$sup' --interaction_type='activator,other/unknown,n/a,inducer,stimulator' --source_trust_levels='Expert curated' > $supFile") or die "ERROR: cannot get drugs\n";
     }
     if($onc ne ""){
-        !system("$callDgidb --genes='$onc' --interaction_type='inhibitor,suppressor,antibody,antagonist,blocker,other/unknown,n/a' --source_trust_levels='Expert curated' --output='$oncFile'") or die "ERROR: cannot get drugs\n";
+        !system("perl $callDgidb --genes='$onc' --interaction_type='inhibitor,suppressor,antibody,antagonist,blocker,other/unknown,n/a' --source_trust_levels='Expert curated' > $oncFile") or die "ERROR: cannot get drugs\n";
     }
     if($other ne ""){
-        !system("$callDgidb --genes='$other' --interaction_type='inhibitor,suppressor,antibody,antagonist,blocker,activator,other/unknown,n/a,inducer,stimulator' --source_trust_levels='Expert curated' --output='$otherFile'") or die "ERROR: cannot get drugs\n";
+        !system("perl $callDgidb --genes='$other' --interaction_type='inhibitor,suppressor,antibody,antagonist,blocker,activator,other/unknown,n/a,inducer,stimulator' --source_trust_levels='Expert curated' > $otherFile") or die "ERROR: cannot get drugs\n";
     }
 }
 
